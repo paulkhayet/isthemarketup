4dff4f6e28d7ef849d548d4273abdeefb850c138
Paul Khayet <paulkhayet@macbookpro.lan>
2024-09-23 15:29:07 -0700

    Designed a rudimentary frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-content">
+        <h1 className="status-text">YES</h1>
+        <p className="percentage-text">0.5%</p>
+        <p className="index-name">S&P 500</p>
+      </div>
+      <div className="other-indexes">
+        <div className="index-card">
+          <h2 className="status-text">YES</h2>
+          <p className="percentage-text">0.3%</p>
+          <p className="index-name">Nasdaq</p>
+        </div>
+        <div className="index-card">
+          <h2 className="status-text">NO</h2>
+          <p className="percentage-text">-0.2%</p>
+          <p className="index-name">Dow Jones</p>
+        </div>
+        <div className="index-card">
+          <h2 className="status-text">YES</h2>
+          <p className="percentage-text">1.0%</p>
+          <p className="index-name">Russell 2000</p>
+        </div>
+      </div>
     </div>
   );
 }
